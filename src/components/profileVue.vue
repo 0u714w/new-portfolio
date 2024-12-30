89c41f03c2b18a27ea24a72dc1dfad9e53696102
Doug Enas <douglassenas@gmail.com>
2024-12-30 18:24:43 -0500

    1.6v

--- src/components/profileVue.vue
+++ src/components/profileVue.vue
@@ -1,37 +1,29 @@
 <template>
-    
-    <b-container>
-     <b-card class="mt-5 rounded shadow-lg">
-          <span class="text-dark font-weight-bold">outlaw</span> <br>
-          <hr>
-          <div>
-    <img src="../assets/projects/doug.GIF" alt="doug" style="max-width: 100%; height: auto;">
-  </div>
-          
-     </b-card>
-      <b-card class="mt-1 rounded shadow-sm">
-          <span class="text-dark font-weight-bold">Personal</span> <br>
-          <hr>
-         <b-badge variant="dark">
-             Doug Enas
-         </b-badge>
-         <br>
-         <b-badge>
-            https://github.com/0u714w            
-         </b-badge>
-         <br>
-         <b-badge variant="dark">
-             Indianapolis, Indiana
-         </b-badge>
-         <br>   
-     </b-card>
- </b-container>
-  
-
+  <b-container>
+    <b-card class="mt-5 rounded shadow-lg">
+      <span class="text-dark font-weight-bold">outlaw</span> <br>
+      <hr>
+      <div>
+        <img src="../assets/projects/doug.gif" alt="Doug's Project" style="max-width: 100%; height: auto;">
+      </div>
+    </b-card>
+    <b-card class="mt-1 rounded shadow-sm">
+      <span class="text-dark font-weight-bold">Personal</span> <br>
+      <hr>
+      <b-badge>
+        https://github.com/0u714w            
+      </b-badge>
+      <br>
+      <b-badge variant="dark">
+        Indianapolis, Indiana
+      </b-badge>
+      <br>   
+    </b-card>
+  </b-container>
 </template>
 
 <script>
 export default {
-    name: 'profileVue'
+  name: 'profileVue'
 }
 </script>
