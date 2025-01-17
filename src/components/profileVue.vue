7f786f91c2a6281465578b21a9984201a2312b33
Doug Enas <douglassenas@gmail.com>
2025-01-17 07:50:10 -0500

    remove github

--- src/components/profileVue.vue
+++ src/components/profileVue.vue
@@ -7,18 +7,7 @@
         <img src="../assets/projects/doug.gif" alt="Doug's Project" style="max-width: 100%; height: auto;">
       </div>
     </b-card>
-    <b-card class="mt-1 rounded shadow-sm">
-      <span class="text-dark font-weight-bold">Personal</span> <br>
-      <hr>
-      <b-badge>
-        <a href="https://github.com/0u714w" target="_blank" style="color: white;">github</a>
-      </b-badge>
-      <br>
-      <b-badge variant="dark">
-        Indianapolis, Indiana
-      </b-badge>
-      <br>   
-    </b-card>
+    
   </b-container>
 </template>
 
